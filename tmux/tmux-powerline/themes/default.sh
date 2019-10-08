4608c5a07fd9aaae8909d3c2f6433217dcd3575a
zkasjoa <joacim.kastberg@tieto.com>
2019-10-08 17:22:48 +0200

    Dunno?

--- tmux/tmux-powerline/themes/default.sh
+++ tmux/tmux-powerline/themes/default.sh
@@ -24,13 +24,13 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		#"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		"hostname 5 15" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
 		"pwd 89 211" \
-    "vcs_branch 29 88" \
+    "vcs_branch 9 0" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
 		"vcs_modified 9 255" \
